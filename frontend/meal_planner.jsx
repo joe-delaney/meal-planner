4801a76d218f4aa6e013ea0534851791b06d5b82
joe-delaney <joseph.delaney546@gmail.com>
2022-05-21 10:17:32 -0400

    finished initial set up

---
+++ frontend/meal_planner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to your Weekly Meal Planner!</h1>, root);
+});
