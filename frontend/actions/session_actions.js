866f15ea5ebc77895db51e0e5c52d7069884a86a
joe-delaney <joseph.delaney546@gmail.com>
2022-06-01 21:50:52 -0400

    finished session actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -4,3 +4,33 @@ export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const REMOVE_CURRENT_USER = "REMOVE_CURRENT_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+const receiveCurrentUser = (currentUser) => ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+});
+
+const logoutCurrentUser = () => ({
+    type: REMOVE_CURRENT_USER
+});
+
+export const receiveErrors = (errors) => ({
+    type: RECEIVE_ERRORS,
+    errors
+});
+
+export const clearErrors = () => ({
+    type: CLEAR_ERRORS
+});
+
+//Login thunk action
+export const login = (user) => dispatch => SessionAPIUtil.login(user)
+    .then(currentUser => dispatch(receiveCurrentUser(currentUser)))
+
+//Signup thunk action
+export const signup = (user) => dispatch => SessionAPIUtil.signup(user)
+    .then(currentUser => dispatch(receiveCurrentUser(currentUser)))
+
+//Logout thunk action
+export const logout = () => dispatch => SessionAPIUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()))
