b913d448d337f246bdbc16c8f71e74e222ab0f2e
joe-delaney <joseph.delaney546@gmail.com>
2022-06-01 21:49:56 -0400

    setting up session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,6 @@
+import * as SessionAPIUtil from "../util/session_api_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const REMOVE_CURRENT_USER = "REMOVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
