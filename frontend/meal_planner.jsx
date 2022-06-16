64148959b6a4d4f8930c0787faf2cd0009823215
joe-delaney <joseph.delaney546@gmail.com>
2022-06-16 13:40:53 -0400

    bootstrapped the current user

--- frontend/meal_planner.jsx
+++ frontend/meal_planner.jsx
@@ -4,8 +4,19 @@ import configureStore from "./store/store";
 import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
-
   const root = document.getElementById("root");
+  let store;
+  if (window.currentUser) {
+      const preloadedState = {
+          entities: {
+              users: { [window.currentUser.id]: window.currentUser }
+          },
+          session: { currentUserId: window.currentUser.id }
+      };
+      store = configureStore(preloadedState);
+      delete window.currentUser;
+  } else {
+      store = configureStore();
+  }
   ReactDOM.render(<Root store={store}/>, root);
 });
